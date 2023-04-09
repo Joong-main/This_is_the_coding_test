61c4baac93962f06840f6ac2f0136af438c69026
Lee Jong In <76873740+bell-person-ii@users.noreply.github.com>
2023-04-09 14:51:34 +0900

    Update 모험가 길드.py

--- 그리디/모험가 길드.py
+++ 그리디/모험가 길드.py
@@ -1,17 +1,15 @@
-'''
+#그룹수만 많이 나오면됨, 공포도가 높은 사람은 나중에 뽑을수록 유리해지는 그리디 문제
 rep = int(input())
 ls=list(map(int,input().split()))
 
 ls.sort()
-ls.reverse()
-group_ls=[]
+res=0
+cnt=0
 
-while True:
-    if len(ls)== 0 or ls[0] == 1 :
-        break
-    new_group = ls[0:ls[0]]
-    group_ls.append(new_group)
-    ls=ls[ls[0]:]
-    
-print(len(group_ls)+len(ls))
-'''
+for i in ls:
+    cnt+=1
+    if cnt >= i:
+        res+=1
+        cnt=0
+        
+print(res)
